0f64be3213ebcb9965063b8772c8951da768d507
pan1dan <panonedan@yandex.ru>
2023-11-12 18:28:15 +0500

    2-nd version

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int userCommand1;
-        int userCommand2;
-        String userAnswer1;
-        String userAnswer2;
-        String userAnswer3;
         TaskData taskData = new TaskData();
         EpicAndSubtaskData epicData = new EpicAndSubtaskData();
     }
